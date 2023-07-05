16431fdd19e1a930271289b328ca8f9b1c19ea77
Yuvraj Timalsina <timalsinayuvraj@gmail.com>
2023-07-05 13:28:00 +0545

    refractor state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,13 +3,23 @@ import memesData from "../memesData.js"
 
 function Meme() {
 
-    const [memeImage, setMemeImage] = useState('')
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [allMemeImages] = useState(memesData)
+
 
     function getMemeImage() {
-        const memesArray = memesData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const imgUrl = memesArray[randomNumber].url
-        setMemeImage(imgUrl)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+
     }
 
     return (
@@ -32,7 +42,7 @@ function Meme() {
                     Get New Meme Image 📸
                 </button>
             </div>
-            <img className='meme-image' src={memeImage} />
+            <img className="meme-image" src={meme.randomImage} />
         </main>
     )
 }
